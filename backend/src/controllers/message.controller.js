e0545e99f63dfbb0635031ef3232a3f88cded84c
Rohith-gande <sunnygande146@gmail.com>
2025-09-06 15:45:04 +0530

    create frontend components

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -35,8 +35,29 @@ export const getMessages=async(req,res)=>{
 
 export const sendMessage=async(req,res)=>{
     try {
-        
+        const {text,image}=req.body;
+        const {id:receiverId}=req.params;
+        const senderId=req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse=await cloudinary.uploader.upload(image);
+            imageUrl=uploadResponse.secure_url;
+        }
+
+        const newMessage=new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+        await newMessage.save();
+
+          //todo : realtime functionality goes here 
+        res.status(201).json(newMessage)
     } catch (error) {
         
+        console.error("Error sending message:", error);
+        return res.status(500).json({ message: "Server error" });
     }
 }
